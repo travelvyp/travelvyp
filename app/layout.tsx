f0d3739ef53cb16332aeccfd8db6319a32e87255
travelvyp <pablo.tocci@gmail.com>
2026-05-20 18:23:11 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const metadata = {
+  title: "TravelVYP",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
